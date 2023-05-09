5fd1fceb764f32597f20174cf68999c5e40cc634
DaddyShark97 <shumikdenys@gmail.com>
2023-05-09 11:39:01 +0300

    Use Retrofit

--- app/src/main/java/com/example/avc_jokesandquotes/JokeApp.kt
+++ app/src/main/java/com/example/avc_jokesandquotes/JokeApp.kt
@@ -2,6 +2,9 @@ package com.example.avc_jokesandquotes
 
 import android.app.Application
 import com.google.gson.Gson
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 class JokeApp : Application() {
 
@@ -9,7 +12,15 @@ class JokeApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
-        viewModel = MainViewModel(BaseModel(JokeService.Base(Gson()), ManageResources.Base(this)))
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://official-joke-api.appspot.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        viewModel = MainViewModel(
+            BaseModel(
+                retrofit.create(JokeService::class.java),
+                ManageResources.Base(this)
+            )
+        )
     }
 }
